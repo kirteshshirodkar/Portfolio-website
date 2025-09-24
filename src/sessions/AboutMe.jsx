cfd514207030f6e30b2be7507a4ab304589a7cf4
kirteshshirodkar <kirteshshirodkar8@gmail.com>
2025-09-24 22:57:47 +0530

    changes in about me page : updated university

--- src/sessions/AboutMe.jsx
+++ src/sessions/AboutMe.jsx
@@ -10,15 +10,19 @@ export default function AboutMe() {
     <div className="bg-[#fff7f3]">
       <Navbar />
       <div className=" text-[#332e2e] text-xl  pt-10 mb-30 ">
-        <h1 className="text-6xl font-bold pl-50 max-sm:px-5 sm:max-xl:pl-5 max-xl:mb-10 "> I'm Kirtesh Shirodkar</h1>
+        <h1 className="text-6xl font-bold pl-50 max-sm:px-5 sm:max-xl:pl-5 max-xl:mb-10 ">
+          {" "}
+          I'm Kirtesh Shirodkar
+        </h1>
         <div className="flex flex-row items-center pl-50 max-sm:px-5 sm:max-xl:pl-5 max-sm:flex-col-reverse ">
           <div className="pr-25 sm:max-xl:pr-5 max-sm:pr-5">
             <h1 className="text-3xl font-medium mb-5 max-sm:mt-10">About Me</h1>
             <p className="mb-10">
               a passionate Front-End Developer based in Goa, India. Currently
-              pursuing my Bachelor of Computer Applications (BCA) at Don Bosco
-              College, Panjim, I have a strong foundation in programming and a
-              keen interest in creating seamless, user-friendly web experiences.
+              pursuing my Masters of Computer Applications (MCA) Garden City
+              Univercity Bengalore, I have a strong foundation in programming
+              and a keen interest in creating seamless, user-friendly web
+              experiences.
             </p>
             <h1 className="text-3xl font-medium mb-5">My Journey</h1>
             <p>
@@ -45,24 +49,24 @@ export default function AboutMe() {
           />
           <div className="pr-25 sm:max-xl:pr-5 max-sm:p-0  ml-20 sm:max-xl:ml-5 max-sm:m-0">
             <h1 className="text-3xl font-medium mb-5">What I Do</h1>
-            
-              <ol className="space-y-5 mb-10">
-                <li>
-                  <b>Freelance Web Development</b> – I design and maintain
-                  client websites, focusing on performance, accessibility, and
-                  aesthetics.
-                </li>
-                <li>
-                  <b>AI Integration</b> – I leverage AI tools to enhance project
-                  development, ensuring efficiency and innovation.
-                </li>
-                <li>
-                  <b>Project Building</b> – Some of my notable projects include
-                  a Nike-themed static website and a Weather App, showcasing my
-                  ability to turn ideas into functional web applications.
-                </li>
-              </ol>
-            
+
+            <ol className="space-y-5 mb-10">
+              <li>
+                <b>Freelance Web Development</b> – I design and maintain client
+                websites, focusing on performance, accessibility, and
+                aesthetics.
+              </li>
+              <li>
+                <b>AI Integration</b> – I leverage AI tools to enhance project
+                development, ensuring efficiency and innovation.
+              </li>
+              <li>
+                <b>Project Building</b> – Some of my notable projects include a
+                Nike-themed static website and a Weather App, showcasing my
+                ability to turn ideas into functional web applications.
+              </li>
+            </ol>
+
             <h1 className="text-3xl font-medium mb-5">My Vision</h1>
             <p>
               I strive to build intuitive and visually appealing digital
@@ -71,7 +75,6 @@ export default function AboutMe() {
               performance, or experimenting with new frameworks, I’m always
               excited to push my limits.
             </p>
-           
           </div>
         </div>
       </div>
